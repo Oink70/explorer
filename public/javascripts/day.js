8734da95c136159448b1da41193f7a9c0090ca3a
0x03 <0x03-ctrlc@protonmail.com>
2020-12-23 00:47:28 +0100

    moved day.js initialization to seperate file

---
+++ public/javascripts/day.js
@@ -0,0 +1,5 @@
+// load dayjs plugins
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+dayjs.extend(window.dayjs_plugin_customParseFormat);
+dayjs.extend(window.dayjs_plugin_advancedFormat);
